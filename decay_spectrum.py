800f863448d3bdbbdb7f32b328a7831b25d3e63f
Kate Pachal <katherine.pachal@cern.ch>
2020-07-31 18:44:48 +0200

    update

--- decay_spectrum.py
+++ decay_spectrum.py
@@ -10,7 +10,7 @@ mass = 200
 energy = 500
 
 # Units in ns
-tau = 1
+taus = [0.01,0.1,1,10,100]
 
 # Optional
 # "isotropic", "central"
@@ -22,15 +22,17 @@ from scipy import constants
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 
+for tau in taus :
+
 # tau is in ns; convert to seconds
 tau_s = tau * constants.nano
+print("Tau in seconds:",tau_s)
 
 ## Mean lifetime tau:
 # N(t) = N(0) exp(-t/tau)
 ## Mean distance xMean:
 # N(x) = N(0) exp(-x/xMean)
-
-# xMean for relativistic particle = beta gamma c tau
+# where xMean for relativistic particle = beta gamma c tau
 
 # Energy is total energy; mass is rest mass in GeV/c^2
 # Gamma = total energy/rest energy, where rest energy = rest mass * c^2
@@ -38,20 +40,29 @@ gamma = float(energy)/float(mass)
 beta = np.sqrt(1.-(1./gamma)**2)
 xMean = beta * gamma * constants.c * tau_s
 print("Velocity is",beta,"of c.")
-print("Gamma factor is",gamma)
-print("xMean is", xMean)
+print("Beta-gamma is",beta*gamma)
+print("ctau is",constants.c * tau_s,"m")
+print("xMean is", xMean,"m")
 
 ## Now create decay distribution.
 def N(x) :
   return np.exp(-x/xMean)
 
-## Probability particle has decayed
+## Probability particle has decayed at location x
 N_total = integrate.quad(N, 0, 200)[0]
 def N_cumulative(x) :
   integral = integrate.quad(N,0,x)[0]
   return integral/N_total
 
+
 ## Eta distribution of particles
+def eta_dist(theta) :
+  # If we're not bothering with this effect, assuming everything is at eta=0
+  if not angular_dist : return 0
+#  elif angular_dist == "isotropic" :
+    
+
+
 #def eta_isotropic(theta) :
   
 
@@ -65,3 +76,4 @@ plt.plot(xvals, N(xvals), 'b', xvals, yvals_cumulative, 'r')
 plt.xlim(6e-6, 200)
 plt.xscale('log')
 plt.show()
+
