bb0c122b4874bf52e8444065b6a20d10154da5ce
Kate Pachal <katherine.pachal@cern.ch>
2020-08-12 15:49:48 +0200

    ready to do detector integrals

--- decay_spectrum.py
+++ decay_spectrum.py
@@ -18,7 +18,7 @@ graph_formatting = {
   "total_dist" : {"xrange": (5e-5,20),
                   "yrange": (0,1),
                   "xlabel": "Total decay displacement [m]",
-                  "ylabel": "Fraction of decays"},
+                  "ylabel": "Relative decay probability"},
   "decayed_by_dist" : {"xrange": (5e-5,20),
                   "yrange": (0,1),
                   "xlabel": "Total decay displacement [m]",
@@ -26,11 +26,35 @@ graph_formatting = {
   "transverse_dist" : {"xrange": (5e-5,20),
                   "yrange": (0,1),
                   "xlabel": "Transverse decay displacement [m]",
-                  "ylabel": "Fraction of decays"},
+                  "ylabel": "Relative decay probability"},
   "dist" : {"xrange": (5e-5,20),
                   "yrange": (0,1),
                   "xlabel": "Decay displacement [m]",
-                  "ylabel": "Fraction of decays"},                  
+                  "ylabel": "Relative decay probability"},
+}
+
+# Everything in m
+detector_info = {
+  "Inner detector" : {"abs_z1" : 0.3315, 
+                      "abs_z2" : 2.72,
+                      "r1" : 0.033,
+                      "r2" : 1.066  },
+  "Calorimeters" : {
+                    # Tile cal: r = 2.3 to 3.9
+                    # total z length: 12m, so abs_z1 = abs_z2 = 6 m
+                    # LAr cal: r = 1.15 to 2.25
+                    # Approximate whole as LAr inner radius to Tile outer radius,
+                    # and 6 m long to include endcaps? -> yes
+                    "abs_z1" : 6.0,
+                    'abs_z2' : 6.0,
+                    "r1" : 1.15,
+                    "r2" : 3.9 },
+  "Muon spectrometer" : {
+                    "abs_z1" : 6.,
+                    "abs_z2" : 13.,
+                    "r1" : 5.,
+                    "r2": 10.,
+  }
 }
 
 def make_graph(xvals, yvals) :
@@ -126,6 +150,7 @@ import matplotlib.pyplot as plt
 import scipy.special as sc
 
 graphs = {}
+detector_data = {}
 
 for tau in taus :
 
@@ -174,21 +199,53 @@ for tau in taus :
   yvals_decayprob = [N_cumulative(x) for x in xvals_distance]
   graphs[tau_string]["decayed_by_dist"] = make_graph(xvals_distance,yvals_decayprob)
 
+  # For transverse distances
+  def integrand_z(z, r, dummy1, dummy2, dummy3, dummy4) :
+    return sc.expi(- np.sqrt(r**2+z**2)/xMean)
+
   ## Probability particle has decayed by *transverse* distance x
   def N_transverse(r) :
-    integrand = lambda z : sc.expi(- np.sqrt(r**2+z**2)/xMean)
     # Appropriate z range for integral depends on lifetime.
     z_end = 5*tau
-    val = integrate.quad(integrand,-z_end,z_end)    
+    val = integrate.quad(integrand_z,-z_end,z_end,args=(r, 0,0,0,0))
     return -val[0]
 
   # Save
   norm_val = N_transverse(xvals_distance[0])
-  print "Normalisation value is",norm_val
   yvals_transversedist = [N_transverse(x)/norm_val for x in xvals_distance]
-  print yvals_transversedist[0:20]
   graphs[tau_string]["transverse_dist"] = make_graph(xvals_distance,yvals_transversedist)
-  
+
+  def lim_z(r, abs_z1, abs_z2, r1, r2) :
+    # z limits for integration come from a straight line between z1, r1, and z2, r2 at r.
+    # Use python's linear interpolation.
+    z = np.interp(r,[r1,r2],[abs_z1,abs_z2])
+    return [-z,z]
+
+
+  def N_subdetector(abs_z1, abs_z2, r1, r2) :
+    args = (abs_z1, abs_z2, r1, r2)
+    val = integrate.nquad(integrand_z,[lim_z,[r1,r2]], args=args)
+    return -val[0]
+
+  # Total in 2D is integral of N_transverse out to appropriate r.
+  # Let's start from value of first encountering detector.
+  N_transverse_total = N_subdetector(5*tau,5*tau,0,5*tau)
+
+  test = N_subdetector(1,1,1,2)
+  print "Frac in subdector from -1 to 1 and 1 to 2:",test/N_transverse_total
+  print "Total:",N_transverse_total
+  print "N in here:",test
+
+  print "Test limit. Interpolate from z=1 at r=1 to z=3 at r=2, evaluate for r=1.5"
+  print "z_lims are",lim_z(1.5,1,3,1,2)
+
+  # Calculation per subdetector
+  for detector in detector_info.keys() :
+
+    dimensions = detector_info[detector]
+    #n_particles = N_subdetector()
+
+
 
 # Now make plots.
 legend_list = ["#tau = {0} ns".format(i) for i in sorted(graphs.keys())]
